5e3516eb60cc74f902fe40399acc80b24010a74f
Marantesss <up201706917@fe.up.pt>
2018-11-17 19:28:22 +0000

    Finished Aula6_jogo

--- Aula6_Jogo/src/Jogo.cpp
+++ Aula6_Jogo/src/Jogo.cpp
@@ -5,6 +5,7 @@
 
 #include "Jogo.h"
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 unsigned Jogo::numPalavras(string frase)
@@ -62,17 +63,20 @@ Crianca& Jogo::perdeJogo(string frase)
 {
 	list<Crianca>::iterator it = criancas.begin();
 	list<Crianca>::iterator it_end = criancas.end();
-
-	while(criancas.size() != 1) {
-		for (int n = this->numPalavras(frase); n > 1; n--) {
+	int numPalavrasFrase = this->numPalavras(frase) - 1;
+	int numCriancas, ultimaVolta;
+	while(criancas.size() > 1) {
+		numCriancas = this->criancas.size();
+		ultimaVolta = numPalavrasFrase%numCriancas;	// Evita voltas
+		for (int i = 0; i < ultimaVolta; i++) {
+			it++;
 			if (it == it_end)
 				it = criancas.begin();
-			else
-				it++;
 		}
-		cout << criancas.size() << endl;
 		cout << "Sai: " <<  it->escreve() << endl;
 		it = criancas.erase(it);
+		if (it == it_end)
+			it = criancas.begin();
 	}
 
 	return *it;
@@ -81,28 +85,82 @@ Crianca& Jogo::perdeJogo(string frase)
 
 list<Crianca>& Jogo::inverte()
 {
-	criancas.reverse();
-	return criancas;
+	//... o QUE ELES QUEREM
+	/*
+	list<Crianca> l1;
+	list<Crianca>::iterator it = criancas.begin();
+	list<Crianca>::iterator ite = criancas.end();
+	while ( it != ite )
+	{
+		Crianca *c=new Crianca(*it);
+		l1.push_front(*c);
+		it ++;
+	}
+	criancas = l1;
+	*/
+
+	//... O QUE AS PESSOAS NORMAIS FAZEM
+	this->criancas.reverse();
+	return this->criancas;
 }
 
 
 list<Crianca> Jogo::divide(unsigned id)
 {
-	// a alterar
-	list<Crianca> res;
-	return res;
+	list<Crianca> returnList;
+	list<Crianca>::iterator it = criancas.begin();
+	// list<Crianca>::iterator it_end = criancas.end();
+	int numCriancas = this->criancas.size();
+	for (int i = 0; i < numCriancas; i++) {
+		if (it->getIdade() > id) {
+			returnList.push_back(*it);
+			it = criancas.erase(it);
+		}
+		else {
+			it++;
+		}
+	}
+	return returnList;
 }
 
 bool Jogo::operator==(Jogo& j2)
 {
-	// a alterar
+	if (this->criancas.size() != j2.criancas.size()) {
+		return false;
+	}
+
+	list<Crianca>::iterator it = this->criancas.begin();
+	list<Crianca>::iterator it_end = this->criancas.end();
+	list<Crianca>::iterator it2 = j2.criancas.begin();
+
+	for (/* nada */; it != it_end; it++, it2++) {
+		if (!(*it == *it2)) { // usa-se !(==) porque o operador != nao esta definido
+			return false;
+		}
+	}
+
 	return true;
 }
 
 
 list<Crianca> Jogo::baralha() const
 {
-	// a alterar
-	list<Crianca> res;
-	return res;
+	list<Crianca> returnList;
+	list<Crianca> trocaCriancas = this->criancas;
+
+	int rPos;
+
+	/* initialize random seed: */
+	srand(time(NULL) );
+
+	while( !trocaCriancas.empty() ){
+		/* generate a random position: */
+		rPos = rand() % trocaCriancas.size();
+		list<Crianca>::iterator it = trocaCriancas.begin();
+		for(int i = 0; i < rPos; i++, it++);	// incrementa o it nPos vezes
+		returnList.push_back(*it);				// adiciona ao returnList
+		trocaCriancas.erase(it);				// essa crianca já esta escolhida
+	}
+
+	return returnList;
 }
