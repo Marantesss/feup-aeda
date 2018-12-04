f3c38f4f75ee6afa8c53041e117d5084fae6b99f
Gonçalo Marantes <up201706917@fe.up.pt>
2018-12-04 18:49:01 +0000

    Added Aula10_Apostas files

---
+++ Aula10_Apostas/src/Aposta.cpp
@@ -0,0 +1,29 @@
+#include "Aposta.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool Aposta::contem(unsigned num) const
+{
+	// TODO
+	return true;
+}
+
+
+void Aposta::geraAposta(const vector<unsigned> & valores, unsigned n)
+{
+	// TODO
+}
+
+
+unsigned Aposta::calculaCertos(const tabHInt & sorteio) const
+{
+	// TODO
+	unsigned certos = 0;
+	return certos;
+}
+
+
+
+
---
+++ Aula10_Apostas/src/Jogador.cpp
@@ -0,0 +1,22 @@
+#include "Jogador.h"
+
+
+void Jogador::adicionaAposta(const Aposta & ap)
+{
+	// TODO
+}
+
+unsigned Jogador::apostasNoNumero(unsigned num) const
+{
+	// TODO
+	unsigned count = 0;
+	return count;
+}
+
+
+tabHAposta Jogador::apostasPremiadas(const tabHInt & sorteio) const
+{
+	// TODO
+	tabHAposta money;
+	return money;
+}
